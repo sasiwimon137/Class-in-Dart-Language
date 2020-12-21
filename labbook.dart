2461da125385da6e8234fe7f8e453cfdd90d2256
sasiwimon137 <sasiwimon2042@gmail.com>
2020-12-21 22:59:39 +0700

    sasi1

--- labbook.dart
+++ labbook.dart
@@ -1,11 +1,13 @@
 void main() {
-  List<Book>book = [];
-  book.add(Book(bookCode: 'B001', bookTitle: 'Java Programming', bookType: 'Textbook', bookPrice: 200));
-  book.add(Book(bookCode: 'B002', bookTitle: 'Beginning Flutter', bookType: 'Textbook', bookPrice: 375));
-  book.add(Book(bookCode: 'B003', bookTitle: 'One Piece', bookType: 'Cartoon', bookPrice: 45));
-  book.add(Book(bookCode: 'B004', bookTitle: 'Naruto', bookType: 'Cartoon', bookPrice: 85));
-  for (var books in book) {
-    print('รหัสหนังสือ' + books.bookCode + 'ชื่อหนังสือ' + books.bookTitle + 'ประเภทหนังสือ' + books.bookType + 'ราคาหนังสือ' + books.bookPrice.toString()); 
+  List<Book>books = [];
+  books.add(Book(bookCode: 'B001', bookTitle: 'Java Programming', bookType: 'Textbook', bookPrice: 200));
+  books.add(Book(bookCode: 'B002', bookTitle: 'Beginning Flutter', bookType: 'Textbook', bookPrice: 375));
+  books.add(Book(bookCode: 'B003', bookTitle: 'One Piece', bookType: 'Cartoon', bookPrice: 45));
+  books.add(Book(bookCode: 'B004', bookTitle: 'Naruto', bookType: 'Cartoon', bookPrice: 85));
+  books.add(Book(bookCode: 'B005', bookTitle: 'C Programing', bookType: 'Textbook', bookPrice: 390));
+  books.add(Book(bookCode: 'B006', bookTitle: 'Doraemon', bookType: 'Cartoon', bookPrice: 85));
+  for (var book in books) {
+    print('รหัสหนังสือ' + book.bookCode + 'ชื่อหนังสือ' + book.bookTitle + 'ประเภทหนังสือ' + book.bookType + 'ราคาหนังสือ' + book.bookPrice.toString()); 
   }
 }
 class Book{
